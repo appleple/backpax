74e87318d12bb9111c3dd9991db870d0a80789bf
atsu666 <ito@appleple.com>
2021-01-21 12:02:09 +0900

    rebuild

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -5,11 +5,17 @@ declare type Control = {
     ratio: number;
     speed: number;
 };
+declare type Item = {
+    element: HTMLElement;
+    insert: HTMLElement;
+};
 export default class Backpax {
     elements: NodeList;
     options: Option;
     controls: Control[];
     move: number;
+    items: Item[];
+    ticking: boolean;
     constructor(selector: string | NodeList, option?: {});
     setBestImg(element: HTMLElement, insert: HTMLElement, index: number): void;
     setup(): void;
